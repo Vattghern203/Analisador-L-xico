27ccfd890cc878e25483f5c8a51f0e9844e19955
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-04-20 20:11:05 -0300

    v.1

--- src/main.py
+++ src/main.py
@@ -25,8 +25,37 @@ from ply.yacc import yacc
 # --- Tokenizer
 
 # All tokens must be named in advance.
-tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
-           'NAME', 'NUMBER' )
+tokens = (
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'NAME',
+    'NUMBER',
+    'TIPO',
+    'INTEIRO',
+    'REAL',
+    'CADEIA_CAR',
+    'PARA',
+    'SE',
+    'SENAO',
+    'ENQUANTO',
+    'ESCREVA',
+    'LEIA',
+    'EQUALS',
+    'COMMA',
+    'COLON',
+    'AND',
+    'OR',
+    'NOT',
+    'LT',
+    'GT',
+    'LE',
+    'GE',
+    'NE',
+)
 
 # Ignored characters
 t_ignore = ' \t'
@@ -39,6 +68,27 @@ t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_TIPO = r'int|real|texto|bool'
+t_INTEIRO = r'([+-])?\d+'
+t_REAL = r'(([+-])?\d+)[.]\d+'
+t_CADEIA_CAR = r'".*?"'
+t_PARA = r'para'
+t_SE = r'se'
+t_SENAO = r'senao'
+t_ENQUANTO = r'enquanto'
+t_ESCREVA = r'escreva'
+t_LEIA = r'leia'
+t_EQUALS = r'=='
+t_COMMA = r','
+t_COLON = r':'
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'!'
+t_LT = r'<'
+t_GT = r'>'
+t_LE = r'<='
+t_GE = r'>='
+t_NE = r'!='
 
 # A function can be used if there is an associated action.
 # Write the matching regex in the docstring.
@@ -54,7 +104,10 @@ def t_ignore_newline(t):
 
 # Error handler for illegal characters
 def t_error(t):
-    print(f'Illegal character {t.value[0]!r}')
+    last_newline_index = t.lexer.lexdata.rfind('\n', 0, t.lexpos)
+    line = t.lexer.lineno
+    column = t.lexpos - last_newline_index
+    print(f"Line {line}, column {column}: Illegal character {t.value[0]!r}")
     t.lexer.skip(1)
 
 # Build the lexer object
@@ -64,17 +117,22 @@ lexer = lex()
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
-def p_expression(p):
+def p_expression_binop(p):
     '''
     expression : term PLUS term
                | term MINUS term
+               | term TIMES term
+               | term DIVIDE term
+               | term LT term
+               | term GT term
+               | term LE term
+               | term GE term
+               | term NE term
+               | term AND term
+               | term OR term
     '''
-    # p is a sequence that represents rule contents.
-    #
-    # expression : term PLUS term
-    #   p[0]     : p[1] p[2] p[3]
-    # 
     p[0] = ('binop', p[2], p[1], p[3])
+
 
 def p_expression_term(p):
     '''
@@ -120,12 +178,34 @@ def p_factor_grouped(p):
     '''
     p[0] = ('grouped', p[2])
 
+def p_factor_string(p):
+    '''
+    factor : CADEIA_CAR
+    '''
+    p[0] = ('string', p[1])
+
+def p_factor_define(p):
+    '''
+    factor : EQUALS
+    '''
+    p[0] = ('equals', p[1])
+
+def p_factor_type(p):
+    '''
+    factor : TIPO
+    '''
+    p[0] = ('type', p[1])
+
 def p_error(p):
-    print(f'Syntax error at {p.value!r}')
+    line = p.lineno
+    column = p.lexpos - lexer.lexdata.rfind('\n', 0, p.lexpos)
+    print(f"Syntax error at line {line}, column {column}: {p.value!r}")
+
 
 # Build the parser
 parser = yacc()
 
 # Parse an expression
-ast = parser.parse('2 * 34.5')
+ast = parser.parse('escreva')
+
 print(ast)
